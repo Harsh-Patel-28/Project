88603e12ae367d49c86870e10714464a2c8ada81
Harsh-Patel-28 <harshp6942@gmail.com>
2025-04-05 19:35:07 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+function off(){
+    document.getElementById("box").style.backgroundColor="white"
+    document.getElementById("text").innerHTML="Turn OFF"
+    document.getElementById("box").style.boxShadow = "0px 0px 0px black";
+
+}
+function on(){
+    document.getElementById("box").style.backgroundColor="lightblue"
+    document.getElementById("text").innerHTML="Turn ON"
+    document.getElementById("box").style.boxShadow = "0px 0px 30px blue";
+
+}
